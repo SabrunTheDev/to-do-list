e7ae74a725b04b3038cf8f2a816a761c116e60d3
Sabrun <officialsabrun@gmail.com>
2024-04-12 01:24:43 -0400

    Updated files

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function ToDoForm(props) {
   const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleChange = (event) => {
     setInput(event.target.value);
@@ -27,6 +32,7 @@ export default function ToDoForm(props) {
         name="text"
         className="todo-input"
         onChange={handleChange}
+        ref={inputRef}
       />
       <button className="todo-button">Add To-Do</button>
     </form>
