f131a5baa6f113482e2c9f836c23a92d90bd548e
Sabrun <officialsabrun@gmail.com>
2024-04-11 23:34:44 -0400

    Updated files

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ToDoForm from "./ToDoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+
+export default function ToDo(toDos, completeToDo, removeToDo) {
+  const [edit, saveEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return toDos.map((todo, index) => {
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeToDo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={() => removeToDo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit />
+      </div>
+    </div>;
+  });
+}
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ToDoForm from "./ToDoForm";
+import ToDo from "./ToDo";
 
 export default function ToDoList() {
   const [toDos, setToDo] = useState([]);
@@ -14,10 +15,21 @@ export default function ToDoList() {
     console.log(...toDos);
   };
 
+  // const completeToDo = (id) => {
+  //   let updatedToDos = toDos.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.isComplete = !todo.isComplete;
+  //     }
+  //     return todo;
+  //   });
+  //   setToDo(updatedToDos);
+  // };
+
   return (
     <div className="">
       <h1>What To Do Today?</h1>
       <ToDoForm onSubmit={addToDo} />
+      <ToDo />
     </div>
   );
 }
